98a593eb0849e4edea7b249430041467bccbc866
saritha odnam <so2170@nyu.edu>
2021-02-27 23:43:24 -0500

    ICMP

--- solution.py
+++ solution.py
@@ -10,11 +10,13 @@ import binascii
 # Should use stdev
 
 ICMP_ECHO_REQUEST = 8
-timeRTT = []
+#timeRTT = []
+timeRTT =[]
 packageSent =0
 packageRev = 0
 packet_min =0
 packet_max =0
+packet_avg=0
 
 
 def checksum(string):
@@ -42,50 +44,70 @@ def checksum(string):
 
 
 def receiveOnePing(mySocket, ID, timeout, destAddr):
-   global packageRev,timeRTT,packet_min,packet_max
+   global packageRev,timeRTT,packet_min,packet_max,packet_avg
    timeLeft = timeout
 
+   global received
+   global roundTripTimeLog
+   timeRemaining = timeout
    while 1:
        startedSelect = time.time()
-       #print ("Startedtime:",startedSelect)
+       # print ("Startedtime:",startedSelect)
        whatReady = select.select([mySocket], [], [], timeLeft)
-       #print("time.time:",time.time())
+       # print("time.time:",time.time())
        howLongInSelect = (time.time() - startedSelect)
-       #print ("howlonginseelct:", howLongInSelect)
+       # print ("howlonginseelct:", howLongInSelect)
        if whatReady[0] == []:  # Timeout
            return "Request timed out."
 
        timeReceived = time.time()
-       #print ("timereceived:", timeReceived)
+       # print ("timereceived:", timeReceived)
        recPacket, addr = mySocket.recvfrom(1024)
-       #print ("recPacket:", recPacket, "Addr:", addr)
+       # print ("recPacket:", recPacket, "Addr:", addr)
        # Fill in start
        icmpHeader = recPacket[20:28]
        icmpType, code, mychecksum, packetID, sequence = struct.unpack("bbHHh", icmpHeader)
-       #print("the header received:"+ icmpType, code, mychecksum, packetID, str(sequence))
-       if type != 8 and packetID == ID:
+       # print("the header received:"+ icmpType, code, mychecksum, packetID, str(sequence))
+       if packetID == ID:
            bytesInDouble = struct.calcsize("d")
            timeSent = struct.unpack("d", recPacket[28:28 + bytesInDouble])[0]
-           #print("timeRTT:",timeRTT)
-           #print ("timeRTT:", timeRTT)
-           #print("timesent:"+timeSent)
-           timeRTT.append(timeReceived - timeSent)
-           packageRev += 1
-           diff = timeReceived - timeSent
-           #print("timeRTT lenght:", len(timeRTT))
-           #print("timeRTT min", min(timeRTT))
-           #print("timeRTT max", max(timeRTT))
-           #print("timeRTT avg", float(sum(timeRTT)/len((timeRTT))))
-           #print("timeRTT stdev" %(statistics.stdev(timeRTT,statistics.mean(timeRTT))))
-           #print("timeRTT stdev", float(math.sqrt(sum(timeRTT)/len(timeRTT) - sum(timeRTT/len(timeRTT)**2))) )
-           packet_min = min(timeRTT) if len(timeRTT) > 0 else 0
-           packet_max = max(timeRTT) if len(timeRTT) > 0 else 0
-           #print(
-            #   "maxRTT:", (max(timeRTT) if len(timeRTT) > 0 else 0), "\tminRTT:", (
-             #      min(timeRTT) if len(timeRTT) > 0 else 0), "\naverageRTT:", float(
-              #     sum(timeRTT) / len(timeRTT) if len(timeRTT) > 0 else float("nan")))
+           #print("timesent", timeSent)
 
-           return packet_min,packet_max
+           # print ("timeRTT:", timeRTT)
+           # print("timesent:"+timeSent)
+           diff = timeReceived - timeSent
+           timeRTT.append(diff)
+           packageRev += 1
+           #print("timeRTT:", timeRTT)
+           # mean = statistics.mean(timeRTT)
+           # print("mean:",mean)
+           # print("timeRTT lenght:", len(timeRTT))
+           # print("timeRTT min", min(timeRTT))
+           # print("timeRTT max", max(timeRTT))
+           #print("timeRTT avg", float(sum(timeRTT)/len((timeRTT))))
+           # print("timeRTT stdev" %(statistics.stdev(timeRTT,xbar=mean)))
+           # print("timeRTT stdev", float(math.sqrt(sum(timeRTT)/len(timeRTT) - sum(timeRTT/len(timeRTT)**2))) )
+           #packet_min = min(timeRTT) if len(timeRTT) > 0 else 0
+           #packet_max = max(timeRTT) if len(timeRTT) > 0 else 0
+           #timeRTT_sum = sum(timeRTT)
+           #print ("timeRTTsum",timeRTT_sum)
+           # print("sum timertt", sum(timeRTT), "len time rtt", len(timeRTT))
+           #print("packageREv", packageRev)
+           #packet_avg=(timeRTT_sum / packageRev)
+           #print("printAVg:", packet_avg)
+           #mean = statistics.mean(timeRTT)
+           #under_root = ((sum(timeRTT)-mean)**2)*1.0/packageRev
+           #print ("standard deviation",math.sqrt(under_root))
+
+
+           # print(
+           #   "maxRTT:", (max(timeRTT) if len(timeRTT) > 0 else 0), "\tminRTT:", (
+           #      min(timeRTT) if len(timeRTT) > 0 else 0), "\naverageRTT:", float(
+           #     sum(timeRTT) / len(timeRTT) if len(timeRTT) > 0 else float("nan")))
+
+           return diff
+       else:
+           return "0: IP Header bad"
 
        # Fetch the ICMP header from the IP packet
 
@@ -156,16 +178,26 @@ def ping(host, timeout=1):
        delay = doOnePing(dest, timeout)
 
       # print ("MaxRTT:",(max(timeRTT) if len(timeRTT)>0 else 0))
+       #vars = [float(round(packet_min, 2)), float(round(packet_avg, 2)), float(round(packet_max, 2))]
+       #print("vars:", vars)
 
        time.sleep(1)  # one second
    #print("packet_min",str(round(packet_min,2)))
    #print("packet_max", str(round(packet_max, 2)))
-   vars = [float(round(packet_min, 2)), float(round(packet_max, 2))]
-   #print("vars:", vars)
+   #print("timeRTT:", timeRTT, "sum of timeRTT:", sum(timeRTT))
+   packet_min = min(timeRTT) if len(timeRTT) > 0 else 0
+   packet_max = max(timeRTT) if len(timeRTT) > 0 else 0
+   packet_avg = (sum(timeRTT) / packageRev)
+   mean = statistics.mean(timeRTT)
+   under_root = ((sum(timeRTT) - mean) ** 2) * 1.0 / packageRev
+   # print ("standard deviation",math.sqrt(under_root))
+   stdev_var = math.sqrt(under_root)
+   vars = [float(round(packet_min, 2)),float(round(packet_avg,2)), float(round(packet_max, 2)), float(round(stdev_var,2))]
+   #print(vars)
    return vars
 
 if __name__ == '__main__':
    ping("google.co.il")
-    #ping("127.0.0.1")
+   #ping("173.12.11.12")
 
 
