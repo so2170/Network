6e2097739ecfd2dfa00b8e8d5d3ba79caa000ce3
saritha odnam <so2170@nyu.edu>
2021-04-01 20:22:25 -0400

    Traceroute

--- solution.py
+++ solution.py
@@ -199,7 +199,7 @@ def get_route(hostname):
                     tracelist2.append(tracelist1)
                     #print(destAddr)
                     #print(tracelist2)
-                    return tracelist2
+                    return(tracelist2)
                     #You should add your responses to your lists here and return your list if your destination IP is met
                     #Fill in end
                 else:
